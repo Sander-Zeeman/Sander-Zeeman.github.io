f4a17088bc6c8ec40921d914b508192f27e1d55d
Sander Zeeman <szeeman2000@gmail.com>
2023-10-21 23:21:13 +0200

    Attempt at Intro page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import IntroView from "@/views/IntroView.vue";
+import AboutMeView from "@/views/AboutMeView.vue";
+import PortfolioView from "@/views/PortfolioView.vue";
+import TimelineView from "@/views/TimelineView.vue";
 </script>
 
 <template>
-  <h1>Hello!</h1>
+  <IntroView />
+  <AboutMeView />
+  <PortfolioView />
+  <TimelineView />
+  <div style="position: relative">
+    <span class="footer">&copy;2023 Sander Zeeman</span>
+  </div>
 </template>
 
 <style scoped>
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  bottom: 0;
+  padding-top: 3px;
+  padding-bottom: 3px;
+
+  background-color: #111111;
+  width: 100vw;
+}
 </style>
---
+++ src/views/IntroView.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import LinkedInIcon from "@/assets/linkedin_icon.svg";
+import GithubIcon from "@/assets/github_icon.svg";
+import DiscordIcon from "@/assets/discord_icon.svg";
+import MailIcon from "@/assets/gmail_icon.svg";
+
+import Logo from "@/components/Logo.vue";
+</script>
+
+<template>
+  <div class="container column centered">
+    <img
+      src="https://h-o-m-e.org/wp-content/uploads/2022/04/Blank-Profile-Picture-1.jpg"
+      alt="A picture of me"
+      class="item profile-pic"
+    />
+    <h1 class="name">Sander Zeeman</h1>
+    <h2 class="subtext">What am I</h2>
+    <div class="row centered links">
+      <Logo
+        link="https://www.linkedin.com/in/sanderzeeman/"
+        :icon="LinkedInIcon"
+        alt="LinkedIn"
+      />
+      <Logo link="https://github.com/Sander-Zeeman" :icon="GithubIcon" alt="Github" />
+      <Logo link="#" :icon="DiscordIcon" alt="Discord" />
+      <Logo link="mailto:szeeman2000@gmail.com" :icon="MailIcon" alt="Mail" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.name {
+  font-size: 3em;
+  padding-top: 40px;
+  padding-bottom: 5px;
+}
+
+.subtext {
+  font-size: 2em;
+  padding-top: 5px;
+  padding-bottom: 10px;
+}
+
+.links {
+  padding-top: 10px;
+}
+
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+}
+
+.row {
+  width: 70%;
+  gap: 10px;
+  display: flex;
+  flex-direction: row;
+}
+
+.centered {
+  justify-content: center;
+  align-items: center;
+}
+
+.profile-pic {
+  width: 100px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+}
+</style>
